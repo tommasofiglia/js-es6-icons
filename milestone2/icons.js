e549fba2c8bee7931fabefe1f797c831ec161d41
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-10 15:31:20 +0100

    Commit iniziale

---
+++ milestone2/icons.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Qui le icone che abbiamo definito nella milestone 1
+
+  // definiamo dei colori per le icone (blue, orange, purple)
+
+
+  //aggiungiamo dei colori usando una funzione
+
+
+  //inseriamo le icone colorate nel container
+
+});
+
+
+/* ---- FUNCTIONS ----*/
+//1. Funzione milestone 1
+
+
+//2. funzione per assegnare un colore ad un icona
+// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map 
+
+
+//3. funzione per ottenere da un array
+// tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
---
+++ milestone3/icons.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+ // icone come da milestone 1
+
+
+  // colori come da milestone 2
+ 
+
+  //aggiungiamo dei colori come da milestone 2
+
+  //inseriamo le icone nel container come da milestone 2
+
+  //estrapoliamo i tipi di icone
+
+  //aggiungiamo i tipi alla select
+
+  //al change mostriamo solo le icone filtrate
+  //mostriamo come passare un parametro a change e contemporaneamente destrutturiamo
+
+
+
+/* ---- FUNCTIONS ----*/
+
+});
