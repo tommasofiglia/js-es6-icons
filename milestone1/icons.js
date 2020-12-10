aae527e70cdcfa8a89c9abcd0f30e14b68550e3b
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-10 17:32:32 +0100

    Milestone1 completata

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,15 +1,62 @@
 $(document).ready(function () {
-  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+/*********************************************
+Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
+**********************************************/
 
-  // Semezioniamo il container icons
+  //Per creare questo array di oggetti, ho deciso di usare il constructor e rendere la sintassi più semplice
 
-  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+  class icon {
+    constructor(name, prefix, type, family) {
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family;
+    }
+  };
+
+  //Creazione dell'array di oggetti icone
+  let arrayIcone = [
+    new icon("CAT" , "fas" , "fa-cat" , "animals" ),
+    new icon("CROW" , "fas" , "fa-crow" , "animals" ),
+    new icon("DOG" , "fas" , "fa-dog" , "animals" ),
+    new icon("DOVE" , "fas" , "fa-dove" , "animals" ),
+    new icon("DRAGON" , "fas" , "fa-dragon" , "animals" ),
+    new icon("HORSE" , "fas" , "fa-horse" , "animals" ),
+    new icon("HIPPO" , "fas" , "fa-hippo" , "animals" ),
+    new icon("FISH" , "fas" , "fa-fish" , "animals" )
+  ];
+
+  console.log(arrayIcone);
+
+/***********************************************
+Selezioniamo il container icons
+************************************************/
+
+  //Seleziono il container icons usando jquery
+  const iconsDiv = $('.icons');
+
+  console.log(iconsDiv);
+
+
+/*****************************************************
+ Inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+*****************************************************/
+
+  //Creo una funzione con ciclo forEach che cicli tra gli oggetti e inserisca nel markup le icons
+
+  arrayIcone.forEach(icons => {
+    let iconName = icons.name;
+    let iconPrefix = icons.prefix;
+    let iconType = icons.type;
+    let iconFamily = icons.family;
+
+    let markup = `
+    <div>
+      <i class="${iconPrefix} ${iconType}" style="color:black"></i>
+      <div class="title">${iconName}</div>
+    </div>
+    `
+    iconsDiv.append(markup);
+  });
 
 });
-
-
-/* ---- FUNCTIONS ----*/
-// Creiamo una funzione per iterare nell'array e che appenda al container le icone.
-// tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
-// tip: il template literal ci puo aiutare con il markup
-
